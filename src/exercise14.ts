b2b1cc90005b100d605240ce8198723de7c2d2ca
OwanBerhe <oberhe1@charlotte.edu>
2026-09-02 16:31:30 -0400

    Exercise 14 passed

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,20 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const requests = postIds.map(async (id) => {
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+    );
+
+    const data: PostItem = await response.json();
+
+    return data;
+  });
+  
+  return Promise.all(requests);
 }
